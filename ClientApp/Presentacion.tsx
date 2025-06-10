e3eb133a134ad479ddd885ed0f05840b4bc0bc48
ZairaZu08 <zairalizeth.zunigabravo.gen23@cbtis105.edu.mx>
2025-06-10 00:04:20 -0600

    avance proyecto

---
+++ ClientApp/Presentacion.tsx
@@ -0,0 +1,16 @@
+const Presentacion = () => {
+    // Manejo de Datos
+   
+    //Vista
+    return (
+        <>
+           <div className="display-">Centro de Bachillerato Tecnologico Industrial y de Servicios No. 105</div>
+           <div className="h1">Integrantes</div>
+           <div className="h2">Zaira Lizeth Zuñiga Bravo</div>
+           <div className="h2">Atala Jareny Bustamante Medellin</div>
+           <div className="h1">P3A06 - Proyecto Parcial 3: Configuración del Frontend</div>
+        </>
+    )
+}
+
+export default Presentacion;
